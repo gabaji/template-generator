20d36c1de862633574a6de01139030dd5be8ce18
Ayush Gaba <ayushgaba71@gmail.com>
2021-09-22 16:43:44 +0530

    added DSU

---
+++ DSU.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define ll long long
+#define vi vector<int> 
+#define pii pair<int , int>
+#define pb push_back
+#define sz(x) (int)x.size()
+
+const int inf = 1e9;
+const int mod = 1e9 + 7;
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+
+class DSU{
+	vi e;
+	public:
+	DSU(int n){
+		e = vi(n , -1);
+	}
+	int get(int a){
+		return (e[a] < 0 ? a : e[a] = get(e[a]));
+	}
+	void merge(int a , int b){
+		int p1 = get(a) , p2 = get(b);
+		if(p1 == p2) return;
+		if(e[p1] > e[p2]) swap(p1 , p2);
+		e[p1] += e[p2];
+		e[p2] = p1;
+	}
+	int get_size(int a){
+		return -e[get(a)];
+	} 
+};
+
+int main(){
+	ios_base::sync_with_stdio(false), cin.tie(NULL);
+
+}
